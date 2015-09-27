85b7828f5dc4403d32f6f5b689a44c6f89eda5e2
Ebisu77 <sefc.mario@gmail.com>
2015-09-27 10:57:51 +0200

    Initial commit
    Adding of basic project

---
+++ RoRAssist/Pages/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoRAssist.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            DefaultPage newPage = new DefaultPage();
+            this.NavigationService.Navigate(newPage);
+        }
+
+        private void quitButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
