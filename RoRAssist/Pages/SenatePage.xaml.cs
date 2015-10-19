88d018fc60a1dfabf5ab1a690f55ab34b38e63fa
Ebisu77 <sefc.mario@gmail.com>
2015-10-19 17:42:10 +0200

    Senate page - input of basic votes

--- RoRAssist/Pages/SenatePage.xaml.cs
+++ RoRAssist/Pages/SenatePage.xaml.cs
@@ -29,7 +29,10 @@ namespace RoRAssist.Pages
 
         //data support
         int playersCount;
-        string[] playerNames = new string[6];        
+        string[] playerNames = new string[6];
+        int[] playersOratory = new int[6];
+        int[] playersKnights = new int[6];
+        int[] playersTalents = new int[6];
 
         #endregion
 
@@ -48,6 +51,26 @@ namespace RoRAssist.Pages
         #endregion
 
         #region Private Methods
+
+        /// <summary>
+        /// Retrieve number of players and names of players from xml
+        /// </summary>
+        private void retrievePlayerData()
+        {
+            //create an instance of xml document
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(@"C:\Users\Anakin\OneDrive\Programming\Repo\RoRAssist\RoRAssist\Data\Players.xml");
+
+            //retrieve number of players
+            playersCount = Convert.ToInt32(xmlDoc.SelectSingleNode("//numberOfPlayers").InnerText);
+
+            //retrieve player names and save them into array
+            for (int i = 0; i < playersCount; i++)
+            {
+                XmlElement element = xmlDoc.SelectSingleNode("//*[@playerID='" + i + "']") as XmlElement;
+                playerNames[i] = element.InnerText;
+            }
+        }
 
         /// <summary>
         /// Display results in UI
@@ -101,33 +124,53 @@ namespace RoRAssist.Pages
                 labelPlayer_6.DataContext = playerNames[5];
         }
 
-        /// <summary>
-        /// retrieve names of players from xml
-        /// </summary>
-        private void retrievePlayerData()
-        {
-            //create an instance of xml document
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(@"C:\Users\Anakin\OneDrive\Programming\Repo\RoRAssist\RoRAssist\Data\Players.xml");
-
-            //retrieve number of players
-            playersCount = Convert.ToInt32(xmlDoc.SelectSingleNode("//numberOfPlayers").InnerText);
-
-            //retrieve player names and save them into array
-            for (int i = 0; i < playersCount; i++)
-            {
-                XmlElement element = xmlDoc.SelectSingleNode("//*[@playerID='" + i + "']") as XmlElement;
-                playerNames[i] = element.InnerText;
-            }           
-        }
-
         #endregion
 
         #region Events
 
         private void OnValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
+            //save players oratory into array
+            if (oratoryUpDown_player1 != null)
+                playersOratory[0] = (int)oratoryUpDown_player1.Value;
+            if (oratoryUpDown_player2 != null)
+                playersOratory[1] = (int)oratoryUpDown_player2.Value;
+            if (oratoryUpDown_player3 != null)
+                playersOratory[2] = (int)oratoryUpDown_player3.Value;
+            if (oratoryUpDown_player4 != null)
+                playersOratory[3] = (int)oratoryUpDown_player4.Value;
+            if (oratoryUpDown_player5 != null)
+                playersOratory[4] = (int)oratoryUpDown_player5.Value;
+            if (oratoryUpDown_player6 != null)
+                playersOratory[5] = (int)oratoryUpDown_player6.Value;
 
+            //save number of player knighs into array
+            if (knightsUpDown_player1 != null)
+                playersKnights[0] = (int)knightsUpDown_player1.Value;
+            if (knightsUpDown_player2 != null)
+                playersKnights[1] = (int)knightsUpDown_player2.Value;
+            if (knightsUpDown_player3 != null)
+                playersKnights[2] = (int)knightsUpDown_player3.Value;
+            if (knightsUpDown_player4 != null)
+                playersKnights[3] = (int)knightsUpDown_player4.Value;
+            if (knightsUpDown_player5 != null)
+                playersKnights[4] = (int)knightsUpDown_player5.Value;
+            if (knightsUpDown_player6 != null)
+                playersKnights[5] = (int)knightsUpDown_player6.Value;
+
+            //save players talents into array
+            if (talentsUpDown_player1 != null)
+                playersTalents[0] = (int)talentsUpDown_player1.Value;
+            if (talentsUpDown_player2 != null)
+                playersTalents[1] = (int)talentsUpDown_player2.Value;
+            if (talentsUpDown_player3 != null)
+                playersTalents[2] = (int)talentsUpDown_player3.Value;
+            if (talentsUpDown_player4 != null)
+                playersTalents[3] = (int)talentsUpDown_player4.Value;
+            if (talentsUpDown_player5 != null)
+                playersTalents[4] = (int)talentsUpDown_player5.Value;
+            if (talentsUpDown_player6 != null)
+                playersTalents[5] = (int)talentsUpDown_player6.Value;
         }
 
         #endregion
